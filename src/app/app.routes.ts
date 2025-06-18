8ba5ca0b51d0587b4c53927fd3c4b6be794886b0
divaprates <divasilvaprates@gmail.com>
2025-06-18 17:20:15 -0300

    feat: Preparando o Ambiente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Produtos } from './components/produtos/produtos';
+import { Sobre } from './components/sobre/sobre';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        component: Home
+    },
+    {
+        path: 'sobre',
+        component: Sobre
+    },
+    {
+        path: 'produtos',
+        component: Produtos
+    }
+];
